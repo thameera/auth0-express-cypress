4ce92903b31bb2b9976c9318a5b09c6894fec8e4
Thameera Senanayaka <me@thameera.com>
2020-05-06 21:20:19 +1000

    Implement Auth0

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 require('dotenv').config()
 
 const express = require('express')
+const { auth, requiresAuth } = require('express-openid-connect')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use(auth({ required: false }))
+
+app.use((req, res, next) => {
+  res.locals.user = req.openid.user
+  next()
+})
+
 app.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/dashboard')
+  }
   res.render('landing')
+})
+
+app.get('/dashboard', requiresAuth(), (req, res) => {
+  res.render('dashboard')
 })
 
 const PORT = process.env.PORT || 8081
